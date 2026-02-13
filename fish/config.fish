4e3e9f3cb68105de79b3df2103c2d96ac421440e
Bowlofneighs <bowlofneighs@gmail.com>
2026-02-13 16:38:46 -0500

    basic config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,17 @@
 if status is-interactive
-	# put starting commands here
+	fastfetch
+
+	alias cat "echo !! bat is recommended over cat !! ; echo; echo; /usr/sbin/cat"
+	alias ls "lsd -l"
+	alias .. "z .."
+	alias ... "z ~"
+
+	# zoxide set up
+	alias cd "z"
+	zoxide init fish | source
+	
 end
+
+# terminal-wakatime setup
+set -x PATH "$HOME/.wakatime" $PATH
+terminal-wakatime init fish | source
